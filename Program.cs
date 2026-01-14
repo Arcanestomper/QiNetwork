0e6324150ad33c651830df73f377e88ff9e47032
Arcanestomper <bookworm2038@gmail.com>
2026-01-14 01:19:25 -0500

    Initial Files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QiNetwork;
+using System.Drawing;
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Point current_location = new Point();
+        current_location.X = 0;
+        current_location.Y = 0;
+
+        Qi_Network ChosenOne = new Qi_Network();
+
+        //Initialize Dantian
+        ChosenOne.Nodes[0] = new Node(0, "Dantian", current_location);
+        ChosenOne.Nodes[0].Generated_Qi.Qi_Type.Add("Pure", 10);
+
+        //Create Meridian
+        current_location.X = 1;
+        current_location.Y = 1;
+        ChosenOne.Nodes[1] = new Node(1, "Meridian", current_location);
+        ChosenOne.Nodes[1].Generated_Qi.Qi_Type.Add("Pure", 10);
+        ChosenOne.Connections.Add(new Connection(0, 1));
+
+        //Cycle_Qi
+        var v1 = new Qi_Vector(ChosenOne.Nodes[1].Position.X - ChosenOne.Nodes[0].Position.X, 
+               ChosenOne.Nodes[1].Position.Y - ChosenOne.Nodes[0].Position.Y);
+        Console.WriteLine(v1);
+
+        Console.WriteLine(ChosenOne.Nodes[0]);
+    }
+}
---
+++ QiNetwork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Text;
+
+namespace QiNetwork
+{
+    public class Qi_Network
+    {
+        public Node?[] Nodes { get; set; } = new Node[10];
+
+        public List<Connection?> Connections { get; set; } = new List<Connection?>();
+
+
+    }
+
+    public class Node (int Id, string Name, Point coordinates)
+    {
+        [Required]
+        public int _Id { get; set; } = Id;
+
+        public string? _Name { get; set; } = Name;
+        public Point Position { get; set; } = coordinates;
+
+        public Qi Generated_Qi { get; set; } = new Qi();
+
+        public Qi Current_Qi { get; set; } = new Qi();
+
+        public class Qi
+        {
+            public Dictionary<string, double> Qi_Type { get; set; } = new Dictionary<string, double>();
+        }
+
+    }
+
+    public class Connection (int Start, int End)
+    {
+        public int Node_Id_Start { get; set; } = Start;
+        public int Node_Id_End { get; set; } = End;
+    }
+
+    public class Qi_Vector(int X, int Y)
+    {
+        public int _X { get; set; } = X;
+        public int _Y { get; set; } = Y;
+    }
+
+}
